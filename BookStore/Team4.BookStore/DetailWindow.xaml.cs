340284fa2ed74967478702df91cb101f8d54611c
Dương Trung Quốc <quocbamt@gmail.com>
2025-12-16 15:39:21 +0700

    fix bug save

--- BookStore/Team4.BookStore/DetailWindow.xaml.cs
+++ BookStore/Team4.BookStore/DetailWindow.xaml.cs
@@ -49,6 +49,7 @@ namespace Team4.BookStore
                 QuantityTextBox.Text = X.Quantity.ToString();
                 PriceTextBox.Text = X.Price.ToString();
                 AuthorTextBox.Text = X.Author.ToString();
+                PublicationDataPicker.Text = X.PublicationDate.ToString();
 
                 // phải nhảy đến đúng phần edit mode
                 CategoryIdComboBox.SelectedValue = X.CategoryId;
@@ -69,7 +70,7 @@ namespace Team4.BookStore
                 MessageBox.Show("Name is required", "Required", MessageBoxButton.OK, MessageBoxImage.Stop);
                 return false;
             }
-            if (name.Length < 5 || name.Length > 50)
+            if (name.Length < 5 || name.Length > 100)
             {
                 MessageBox.Show("Name must be 5 to 50 character length", "Validation", MessageBoxButton.OK, MessageBoxImage.Stop);
                 return false;
@@ -95,7 +96,7 @@ namespace Team4.BookStore
             obj.Quantity = int.Parse(QuantityTextBox.Text);
             obj.Price = double.Parse(PriceTextBox.Text);
             obj.Author = AuthorTextBox.Text;
-
+            obj.PublicationDate = PublicationDataPicker.SelectedDate.Value;
             obj.CategoryId = int.Parse(CategoryIdComboBox.SelectedValue.ToString());
             if (X == null) _bookService.CreateBook(obj);
             else _bookService.UpdateBook(obj);
